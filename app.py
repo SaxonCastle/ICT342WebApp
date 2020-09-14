9ffcc77f80ed7f0ff67a206690fd45dd52ca8a65
Saxon <scastle16@gmail.com>
2020-09-14 18:26:13 +1000

    -Cleaned the code a little bit -Added a little error handing

--- app.py
+++ app.py
@@ -3,9 +3,14 @@ import ftplib
 import time
 
 #domain name or server ip:
-session = ftplib.FTP()
-session.connect('139.162.15.145', 2121)
-session.login('user', 'password')
+try:
+    session = ftplib.FTP()
+    session.connect('139.162.15.145', 2121)
+    session.login('user', 'password')
+    print("Login to " + str(139) + " Successful")
+except:
+    print("Login Failed")
+
 
 app = Flask(__name__)
 
@@ -21,6 +26,8 @@ def begin_test():
     protocol = request.form['protocol']
     server = request.form['server']
     packet_size = request.form['packet']
+    running_test = 0.0
+
 
     if server == "172.105.191.25":
         server_location = "Sydney"
@@ -30,26 +37,29 @@ def begin_test():
 
     elif server == "45.33.27.236":
         server_location = "Texas"
-
     else:
         server_location = "Unknown"
 
-    if protocol == "FTP":
-        filename = 'texttest.txt'
-        start = time.perf_counter()
-        session.storbinary('STOR ' + filename, open(filename, 'rb'))
-        end = time.perf_counter()
-        session.quit()
-        time_taken = end - start
-        running_test = round(time_taken, 3)
-        return render_template('home.html', running_test=running_test, protocol=protocol, server=server,
+    # if protocol == "":
+    filename = 'texttest.txt'
+    start = time.perf_counter()
+    session.storbinary('STOR ' + filename, open(filename, 'rb'))
+    end = time.perf_counter()
+    session.quit()
+    time_taken = end - start
+    running_test = round(time_taken, 3)
+
+
+    return render_template('home.html', running_test=running_test, protocol=protocol, server=server,
                                server_location=server_location, packet_size=packet_size)
 
-    elif protocol == "STCP":
-        filename = 'texttest.txt'
+    # elif protocol == "STCP":
+    #     filename = 'texttest.txt'
+    #
+    # else:
+    #     filename = 'texttest.txt'
 
-    else:
-        filename = 'texttest.txt'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
